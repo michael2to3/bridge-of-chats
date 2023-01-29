3d42544308f97cd37d65e61eaf6a2090f12ddcb8
Michael <michael2ga3@gmail.com>
2023-01-29 23:26:44 +0300

    Add reverse link to vk

--- app/src/main/java/bridge/chats/BridgeOfChats.java
+++ app/src/main/java/bridge/chats/BridgeOfChats.java
@@ -2,16 +2,18 @@ package bridge.chats;
 
 import bridge.chats.Platform.TelegramPlatform;
 import bridge.chats.Platform.VkPlatform;
-
-import bridge.chats.Utils.*;
+import bridge.chats.Utils.Config;
 
 class BridgeOfChats {
     private final static Config config = new Config();
 
     public static void run() {
-        VkPlatform vk = new VkPlatform(config.get("vk.token"));
+        VkPlatform vk = new VkPlatform(config.get("vk.token"), Integer.parseInt(config.get("vk.groupid")));
         TelegramPlatform telegram = new TelegramPlatform("test", config.get("telegram.token"));
-        PlatformLinker linker = new PlatformLinker(vk, telegram);
-        linker.start();
+        Linker vk2tg = new Linker(vk, telegram, "-218522752", "-495016909");
+        Linker tg2vk = new Linker(telegram, vk, "-495016909", "-218522752");
+        var manager = new LinkerManager();
+        manager.startLinker(vk2tg);
+        manager.startLinker(tg2vk);
     }
 }
