69c5bd4e97d172a949558d37bf6954b591d51324
Michael <michael2ga3@gmail.com>
2023-01-29 17:51:04 +0300

    Create Config class, allows get easy env - First check system env - Second check JVM env - Last check file prop Clear template config.properties

--- app/src/main/java/bridge/chats/BridgeOfChats.java
+++ app/src/main/java/bridge/chats/BridgeOfChats.java
@@ -3,13 +3,14 @@ package bridge.chats;
 import bridge.chats.Platform.TelegramPlatform;
 import bridge.chats.Platform.VkPlatform;
 
+import bridge.chats.Utils.*;
+
 class BridgeOfChats {
-    private final static String VK_TOKEN = System.getenv("VK_TOKEN");
-    private final static String TELEGRAM_TOKEN = System.getenv("TELEGRAM_TOKEN");
+    private final static Config config = new Config();
 
     public static void run() {
-        VkPlatform vk = new VkPlatform(VK_TOKEN);
-        TelegramPlatform telegram = new TelegramPlatform("test", TELEGRAM_TOKEN);
+        VkPlatform vk = new VkPlatform(config.get("vk.token"));
+        TelegramPlatform telegram = new TelegramPlatform("test", config.get("telegram.token"));
         PlatformLinker linker = new PlatformLinker(vk, telegram);
         linker.start();
     }
---
+++ app/src/test/java/bridge/chats/Utils/ConfigTest.java
@@ -0,0 +1,50 @@
+package bridge.chats.Utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ConfigTest {
+  private Config config;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    String content = "telegram.token=abc\n"
+        + "proxy.host=example.com\n"
+        + "proxy.port=8080\n";
+    var bytes = content.getBytes(StandardCharsets.UTF_8);
+    InputStream input = new ByteArrayInputStream(bytes);
+    config = new Config(input);
+  }
+
+  @Test
+  public void testGetProperty() {
+    System.clearProperty("TELEGRAM_TOKEN");
+    System.clearProperty("PROXY_HOST");
+    System.clearProperty("PROXY_PORT");
+    assertEquals("abc", config.get("telegram.token"));
+    assertEquals("example.com", config.get("proxy.host"));
+    assertEquals("8080", config.get("proxy.port"));
+  }
+
+  @Test
+  public void testGetPropertyWithEnvironmentVariable() {
+    System.setProperty("TELEGRAM_TOKEN", "def");
+    System.setProperty("PROXY_HOST", "example2.com");
+    System.setProperty("PROXY_PORT", "9090");
+    assertEquals("def", config.get("telegram.token"));
+    assertEquals("example2.com", config.get("proxy.host"));
+    assertEquals("9090", config.get("proxy.port"));
+  }
+
+  @Test
+  public void testNotFoundVarible() {
+    assertNull(config.get("something_not_exists"));
+  }
+}
