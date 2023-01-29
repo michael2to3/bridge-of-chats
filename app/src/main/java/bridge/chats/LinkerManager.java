1e34109434b14ea40e075df2fc953a125bed7690
Michael <michael2ga3@gmail.com>
2023-01-29 23:31:23 +0300

    Create manager thread for all Linker Platforms

---
+++ app/src/main/java/bridge/chats/LinkerManager.java
@@ -0,0 +1,38 @@
+package bridge.chats;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkerManager {
+
+    private List<Thread> linkers = new ArrayList<>();
+
+    public LinkerManager() {
+    }
+
+    public void startLinker(Linker linker) {
+        Thread linkerThread = new Thread(new LinkerRunnable(linker));
+        linkerThread.start();
+        linkers.add(linkerThread);
+    }
+
+    public void stopAllLinkers() {
+        for (Thread linker : linkers) {
+            linker.interrupt();
+        }
+        linkers.clear();
+    }
+
+    private class LinkerRunnable implements Runnable {
+        private Linker linker;
+
+        public LinkerRunnable(Linker linker) {
+            this.linker = linker;
+        }
+
+        @Override
+        public void run() {
+            linker.start();
+        }
+    }
+}
